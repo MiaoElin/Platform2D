aaf2a136b42817f58cf05c1a7512e8ea766cb583
MiaoElin <249176332@qq.com>
2024-06-06 22:13:52 +0800

    <feature>add: Panel_Skillslot 倒计时间显示

--- Assets/ScriptRuntime/Business_UI/Panel/Panel_SkillSlot.cs
+++ Assets/ScriptRuntime/Business_UI/Panel/Panel_SkillSlot.cs
@@ -5,9 +5,17 @@ using UnityEngine.UI;
 public class Panel_SkillSlot : MonoBehaviour {
 
     [SerializeField] Image img_Mask1;
+    [SerializeField] Text txt_Time1;
+
     [SerializeField] Image img_Mask2;
+    [SerializeField] Text txt_Time2;
+
     [SerializeField] Image img_Mask3;
+    [SerializeField] Text txt_Time3;
+
     [SerializeField] Image img_Mask4;
+    [SerializeField] Text txt_Time4;
+
 
     float mask1TimeMax;
     float mask2TimeMax;
@@ -23,9 +31,16 @@ public class Panel_SkillSlot : MonoBehaviour {
 
     internal void CD_Tick(float skill1Time, float skill2Time, float skill3Time, float skill4Time) {
         img_Mask1.fillAmount = SetFillAmount(mask1TimeMax, skill1Time);
+        txt_Time1.text = SetTxt_Time(skill1Time);
+
         img_Mask2.fillAmount = SetFillAmount(mask2TimeMax, skill2Time);
+        txt_Time2.text = SetTxt_Time(skill2Time);
+
         img_Mask3.fillAmount = SetFillAmount(mask3TimeMax, skill3Time);
+        txt_Time3.text = SetTxt_Time(skill3Time);
+
         img_Mask4.fillAmount = SetFillAmount(mask4TimeMax, skill4Time);
+        txt_Time4.text = SetTxt_Time(skill4Time);
     }
 
     public float SetFillAmount(float maskTimeMax, float skillTime) {
@@ -33,6 +48,14 @@ public class Panel_SkillSlot : MonoBehaviour {
             return 0;
         } else {
             return skillTime / maskTimeMax;
+        }
+    }
+
+    public string SetTxt_Time(float time) {
+        if (time == 0) {
+            return "";
+        } else {
+            return ((int)time + 1).ToString();
         }
     }
 }
