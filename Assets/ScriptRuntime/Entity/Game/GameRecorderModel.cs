bee9e8dbcd65a7bf83064a86e1d9cf45a8d28e8b
MiaoElin <249176332@qq.com>
2024-06-30 16:53:53 +0800

    <feature> modify: Spr_Map1_BackScene

---
+++ Assets/ScriptRuntime/Entity/Game/GameRecorderModel.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GameRecordModel {
+    public int slotID;
+    public int stageID;
+    public Vector2 ownerPos;
+}
---
+++ Assets/ScriptRuntime/Entity/Game/GameSettingModel.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public class GameSettingModel {
+    public float bgmVolume;
+}
